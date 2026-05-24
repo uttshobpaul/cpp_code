637c625399e220da55e6cfad435771a67668bcf0
UTTSHOB PAUL <uttshobpaul20@gmail.com>
2026-05-24 22:14:31 +0530

    update

--- 35th.cpp
+++ 35th.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<climits>
 using namespace std;
 int main(){
  int mat[2][2];
@@ -13,5 +14,17 @@ cout<<endl;
         cout<<mat[i][j]<<"\t";
     }cout<<endl;
  }
+
+ cout<<endl;
+ int maximum = INT_MIN;
+ for(int i=0;i<2;i++){
+    int sum=0;
+    for(int j=0; j<2;j++){
+     sum += mat[i][j];   
+    }
+    if(sum>=maximum)
+        maximum=sum;
+ }
+ cout<<maximum;
  return 0;
 }
