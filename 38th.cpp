0f3da66734459fca5143971f4566508bf3ed8796
UTTSHOB PAUL <uttshobpaul20@gmail.com>
2026-05-27 19:18:13 +0530

    update

--- 38th.cpp
+++ 38th.cpp
@@ -1,24 +1,36 @@
+//twoSum using hasmaping(uses unordered map)
 #include<iostream>
-#include<algorithm>
+#include<vector>
+#include <unordered_map>
 using namespace std;
-int main(){
-    int arr[]={5,2,11,7,15};
-    int target=9;
-    sort(arr , (arr + (sizeof(arr)/sizeof(arr[0]))));
-    for(int i=0; i<6; i++)
-     cout<<arr[i]<<" ";
-     cout<<endl;
-    int st=0 , end=(sizeof(arr)/sizeof(arr[0]))-1;
-    while (st<end){
-        if((arr[st]+arr[end]) == target){
-            cout<<st<<","<<end;
-           return 0;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int , int> m ;
+    vector<int> ans;
+
+    for(int i=0; i<nums.size(); i++){
+        int first=nums[i];
+        int second = target - first;
+
+        if(m.find(second) != m.end() ){
+            ans.push_back(m[second]);
+            ans.push_back(i);
+            
+            break;
         }
-        else if((arr[st]+arr[end]) > target)
-            end--;
-        else 
-            st++;    
+        m[first]=i;
     }
-        cout<<-1<<","<<-1;
+    return ans;
+    }
+};
+
+int main(){
+    vector<int> arr={5,2,13,7,11,15};
+    int target=9;
+    Solution obj;
+    vector<int> ans=obj.twoSum(arr,target);
+    cout<<ans[0]<<","<<ans[1];
     return 0;
 }
